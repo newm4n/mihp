b21ae910126540de736028d42bf2cf95ddabcada
Ferdinand Neman <ferdinand.neman@gmail.com>
2021-11-05 12:46:34 +0700

    Added graph test

--- central/model/Graph_test.go
+++ central/model/Graph_test.go
@@ -1,7 +1,34 @@
 package model
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGraph_GetGraph(t *testing.T) {
-	// todo finish this
+	g := make(Graph)
+	for i := int64(100); i >= 0; i-- {
+		g[i] = &GraphUnit{
+			Idx: i,
+			Max: i,
+			Min: i,
+			Avg: i,
+			Tot: i,
+			Cnt: 1,
+		}
+	}
+	gs := g.GetGraph(30, 40)
+	assert.Equal(t, 11, len(gs))
+	assert.Equal(t, int64(30), gs[0].Idx)
+	assert.Equal(t, int64(31), gs[1].Idx)
+	assert.Equal(t, int64(30), gs[0].Avg)
+	assert.Equal(t, int64(31), gs[1].Avg)
+
+	gs = g.GetGraph(130, 140)
+	assert.Equal(t, 11, len(gs))
+	assert.Equal(t, int64(130), gs[0].Idx)
+	assert.Equal(t, int64(131), gs[1].Idx)
+	assert.Equal(t, int64(0), gs[0].Avg)
+	assert.Equal(t, int64(0), gs[1].Avg)
+
 }
